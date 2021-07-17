4f7389030488da1c5bea8a73c750d435c169a19b
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2021-07-17 15:54:59 +0530

    set up flask and basic scraping

---
+++ app/file.py
@@ -0,0 +1,18 @@
+from instagramy import InstagramUser
+user = InstagramUser("geeks_for_geeks")
+ 
+# printing the basic details like
+# followers, following, bio
+print(user.is_private)
+print(user.is_verified)
+print(user.number_of_followers)
+print(user.profile_picture_url)
+ 
+# return list of dicts
+posts = user.posts
+
+print('\n\nLikes', 'Comments')
+for post in posts:
+    likes = post.likes
+    comments = post.comments
+    print(likes,comments)
